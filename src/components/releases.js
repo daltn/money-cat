0e93ec575a8aa0c6cff87096b50757f5ae4db702
Dalton Saffe <daltonsaffe@gmail.com>
2019-11-14 00:39:33 -0500

    update site w mcr003

--- src/components/releases.js
+++ src/components/releases.js
@@ -1,33 +1,64 @@
 import React from "react"
 import BlogListing from "./blogListing"
 
-import mcr001Gif from "../images/MCR001.gif"
 import mcr001Img from "../images/MCR001_B.jpg"
 import mcr001FrontImg from "../images/MCR001_A.jpg"
-import mcr002Gif from "../images/MCR002.gif"
 import mcr002Img from "../images/MCR002_B.jpg"
 import mcr002FrontImg from "../images/MCR002_A.jpg"
-
+import mcr003A from "../images/MCR003-A.jpg"
+import mcr003B from "../images/MCR003-B.jpg"
 const Releases = () => {
-  let mcr001Ref = null
-  let mcr002Ref = null
-
   return (
     <div className="column">
       <h1>RELEASES</h1>
+      <h3>MCR003</h3>
+      <a href="https://www.discogs.com/Fine-Touch-Penthouse-Edits-Vol-2/release/14395761">
+        Fine Touch - Penthouse Edits Vol. 2
+      </a>
+      <p>
+        A1 - Mandjo Faya
+        <br />
+        A2 - A Natural High
+        <br />
+        B1 - Pump Party
+        <br />
+        B2 - Candi Flip
+        <br />
+      </p>
+      <section className="grid">
+        <img className="label" src={mcr003A} alt="mcr003 A side" width="300" />
+        <img className="label" src={mcr003B} alt="mcr003 B side" width="300" />
+      </section>
+      <iframe
+        title="Fine Touch - Penthouse Edit Vol 2"
+        width="100%"
+        height="166"
+        scrolling="no"
+        frameBorder="no"
+        allow="autoplay"
+        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/708993118&color=%230a0b0b&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
+      />
       <h3>MCR002</h3>
-      <a href="https://soundcloud.com/moneycatrecs/malik-hendricks-black-chakra-ep-preview">
+      <a href="https://www.discogs.com/Malik-Hendricks-Black-Chakra/release/13504359">
+        Malik Hendricks - Black Chakra
+      </a>
+      <p>
+        A1 - Power
+        <br />
+        A2 - Humanity
+        <br />
+        B1 - Black Chakra
+        <br />
+        B2 - A Circle Forms
+        <br />
+        B3 - A Circle Forms (John Soultrane Remix)
+        <br />
+      </p>
+      <section className="grid">
         <img
           className="label"
-          ref={img => (mcr002Ref = img)}
-          onMouseOver={() => {
-            mcr002Ref.src = mcr002Gif
-          }}
-          onMouseOut={() => {
-            mcr002Ref.src = mcr002FrontImg
-          }}
           src={mcr002FrontImg}
-          alt="mcr001"
+          alt="mcr002 A side"
           width="300"
         />
         <img
@@ -36,30 +67,49 @@ const Releases = () => {
           alt="MCR002-B-Side"
           width="300"
         />
-      </a>
+      </section>
+      <iframe
+        title="MCR 002"
+        width="100%"
+        height="166"
+        scrolling="no"
+        frameBorder="no"
+        allow="autoplay"
+        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/608145264&color=%23111111&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
+      />
       <h3>MCR001</h3>
-      <a href="https://soundcloud.com/moneycatrecs/sets/penthouse-edits-vol-1">
-        <img
-          className="label"
-          ref={img => (mcr001Ref = img)}
-          onMouseOver={() => {
-            mcr001Ref.src = mcr001Gif
-          }}
-          onMouseOut={() => {
-            mcr001Ref.src = mcr001FrontImg
-          }}
-          src={mcr001FrontImg}
-          alt="mcr001"
-          width="300"
-        />
+      <a href="https://www.discogs.com/Fine-Touch-Malik-Hendricks-Penthouse-Edits-Vol1/release/12555593">
+        Fine Touch & Malik Hendricks - Penthouse Edits Vol 1
+      </a>
+      <p>
+        A1 - Fine Touch & Malik Hendricks - Listen
+        <br />
+        A2 - Fine Touch - Want U 4 Myself
+        <br />
+        B1 - Fine Touch & Malik Hendricks - Get it Right
+        <br />
+        B2 - Malik Hendricks - Crying
+        <br />
+      </p>
+      <section className="grid">
+        <img className="label" src={mcr001FrontImg} alt="mcr001" width="300" />
         <img
           className="label"
           src={mcr001Img}
           alt="MCR002-B-Side"
           width="300"
         />
-      </a>
-      <h1>MIXES</h1>
+      </section>
+      <iframe
+        title="pentehouse edits vol 1"
+        width="100%"
+        height="350"
+        scrolling="no"
+        frameBorder="no"
+        allow="autoplay"
+        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/607786554&color=%23282e2f&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
+      />
+      <h1 className="mixes-title">MIXES</h1>
       <BlogListing />
     </div>
   )
